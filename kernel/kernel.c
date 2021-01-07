2a0a8f83448083d2f82fe1aba720be9f34b8cf68
No000 <sk.777.main.9@gmail.com>
2021-01-07 19:27:29 +0900

    add:水平線と垂直線を描画する関数とgraphics.cを追加

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "graphic.h"
 
 /* スケーラブルスクリーンフォントは後日へ */
 /* #define SSFN_CONSOLEBITMAP_HICOLOR /\* use the special renderer for hicolor     \ */
@@ -7,41 +8,9 @@
 /* #include "scalable-font2/ssfn.h" */
 /* extern unsigned char _binary_FreeSerifB_sfn_start; */
 
+  color BLACK = {0x00, 0x00, 0x00};
+
 #define INTEL_ASM_BEGIN ".intel_syntax noprefix\n\t" /* clangの場合.att_syntax prefixは必要ない */
-
-typedef struct VIDEO_INFO {
-  uint8_t *frame_buffer_addr;
-  uint64_t frame_buffer_size;
-  uint32_t horizen_size;
-  uint32_t vertical_size;
-  uint32_t pixel_per_scanline;
-} VIDEO_INFO;
-
-typedef struct pixel_bit_mask{
-  uint8_t blue_mask;
-  uint8_t green_mask;
-  uint8_t red_mask;
-  uint8_t reserved_mask;
-} pixel_bit_mask;
-
-/* カラーを指定するための構造体 */
-typedef struct color{
-  uint8_t blue;
-  uint8_t green;
-  uint8_t red;
-} color;
-
-
-
-/* ピクセル描画関数 */
-void drow_pixel(uint8_t x, uint8_t y, color pixel_color, VIDEO_INFO video_infomation) {
-  pixel_bit_mask *p = (pixel_bit_mask *)video_infomation.frame_buffer_addr;
-  uint32_t hr = video_infomation.horizen_size;
-  pixel_bit_mask *draw_pixel_address = p + (hr * y) + x;
-  draw_pixel_address->blue_mask = pixel_color.blue;
-  draw_pixel_address->green_mask = pixel_color.green;
-  draw_pixel_address->red_mask = pixel_color.red;
-}
 
 void serialport_output(uint8_t ascii_code) {
   __asm__ volatile(INTEL_ASM_BEGIN
@@ -61,7 +30,7 @@ void kernel_main(VIDEO_INFO *video_infomation) {
   int i;
   uint8_t output_data[14] = "kernel_success";
 
-  color BLACK = {0x00, 0x00, 0x00};
+
   for (i = 0; i < 14; i++){
 	serialport_output(output_data[i]);
   }
@@ -73,8 +42,10 @@ void kernel_main(VIDEO_INFO *video_infomation) {
     frame_buffer[i].blue_mask = 0x2f;
   }
   drow_pixel(1, 100, BLACK, *video_infomation);
+		drow_horizon_pixel(100, 100, 500, BLACK, *video_infomation);
+		drow_vertical_pixel(100, 100, 300, BLACK, *video_infomation);
 
-
+		
 /* ssfn_src = (uint8_t *)&_binary_FreeSerifB_sfn_start;      /\* the bitmap font to use *\/ */
 
 /* ssfn_dst.ptr = video_infomation->frame_buffer_addr;                  /\* address of the linear frame buffer *\/ */
