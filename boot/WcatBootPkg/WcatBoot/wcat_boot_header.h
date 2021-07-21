67f12c3bcf50396ca892ca80a19fbaf357117ed5
No000 <sk.777.main.9@gmail.com>
2021-07-21 15:56:49 +0900

    add:smbiosに対するアクセスの追加

--- boot/WcatBootPkg/WcatBoot/wcat_boot_header.h
+++ boot/WcatBootPkg/WcatBoot/wcat_boot_header.h
@@ -26,6 +26,7 @@ typedef struct MEMORY_MAP {
 typedef struct WCAT_HEADER {
     VIDEO_INFO video_information;
     MEMORY_MAP memory_information;
+    uint64_t smbios_address;
 }WCAT_HEADER;
 
 #pragma pack()
