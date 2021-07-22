d7a36c6df8510fc8dffd01a23ebcd46a920ddc08
No000 <sk.777.main.9@gmail.com>
2021-07-22 22:59:26 +0900

    change:informationを追加

--- boot/WcatBootPkg/WcatBoot/menu/menu.c
+++ boot/WcatBootPkg/WcatBoot/menu/menu.c
@@ -7,6 +7,7 @@
 #include "menuLib.h"
 
 #include "settings_menu/settings_menu.h"
+#include "information/information.h"
 
 /* menuの追加方法 */
 /* (1)PUBLIC VOID menu_init(uint32_t cursor_y)関数にメニュー名を記載 */
@@ -16,6 +17,7 @@
 
 VOID boot_menu(uint32_t* stall_flag){
     clear();
+ ret_information:
     logo_print();
     /* init処理をまとめる */
     menu_init(18);
@@ -29,7 +31,6 @@ VOID boot_menu(uint32_t* stall_flag){
         /* グローバル変数が増えるのが嫌だったので、ポインタ渡しで実装 */
         /* ここにsetting_menu.cから戻ってきたときのみ走るinit処理を記載。分岐はenumで定義したflagによる分岐 */
         result_key_data = menu_sentinel(result_key_data, 4, &boot_menu_index);
-        
 
         /* ここですべてのフラグをdisableする処理を記載============================================================== */
         
@@ -39,6 +40,7 @@ VOID boot_menu(uint32_t* stall_flag){
             boot_process_enable(&menu_information);
             shutdown_disable(&menu_information);
             settings_menu_disable(&menu_information);
+            information_disable(&menu_information);
             *stall_flag = 0;    /* stall_flagに関してはsetting_menuで変更したいので、別の構造体を利用する */
             break;
         case 1:
@@ -46,6 +48,7 @@ VOID boot_menu(uint32_t* stall_flag){
             boot_process_enable(&menu_information);
             shutdown_disable(&menu_information);
             settings_menu_disable(&menu_information);
+            information_disable(&menu_information);
             *stall_flag = 1;
             break;
         case 2:
@@ -53,18 +56,21 @@ VOID boot_menu(uint32_t* stall_flag){
             boot_process_disable(&menu_information);
             shutdown_disable(&menu_information);
             settings_menu_disable(&menu_information);
+            information_enable(&menu_information);
             break;
         case 3:
             cursor_print(21);
             boot_process_disable(&menu_information);
             shutdown_disable(&menu_information);
             settings_menu_enable(&menu_information);
+            information_disable(&menu_information);
             break;
         case 4:
             cursor_print(22);
             boot_process_disable(&menu_information);
             shutdown_enabel(&menu_information);
             settings_menu_disable(&menu_information);
+            information_disable(&menu_information);
             break;
         }
         if (result_key_data.UnicodeChar == '\r' && menu_information.boot_process_start_flag == 1){ /* ここ、enumと関数使えばまとめられないかな */
@@ -75,6 +81,9 @@ VOID boot_menu(uint32_t* stall_flag){
         } else if (result_key_data.UnicodeChar == '\r' && menu_information.settings_menu_flag == 1){
             /* settings_menu=================================================== */
             settings_menu();
+        } else if (result_key_data.UnicodeChar == '\r' && menu_information.information_flag == 1){
+            information();
+            goto ret_information;
         }
     }
 }
